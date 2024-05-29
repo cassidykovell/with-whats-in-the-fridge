b6f250becf39421e12881ff8462b1549e1ccbdb9
cassidy <cassidykovell@Cassidys-iMac.lan>
2024-05-29 11:11:25 -0700

    updated profile section

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,28 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const MyProfile = () => {
-  const [activeSection, setActiveSection] = useState('saved');
+  const [activeSection, setActiveSection] = useState("saved");
 
   const renderSection = () => {
-    if (activeSection === 'saved') {
+    if (activeSection === "saved") {
       return <div className="profile-section">Saved Recipes Section</div>;
-    } else if (activeSection === 'created') {
+    } else if (activeSection === "created") {
       return <div className="profile-section">Created Recipes Section</div>;
     }
   };
 
   return (
-    <div className="profile-container">
-      <div className="overlay">
+    <div>
+      <div className="profile-header">
         <h1 className="title">My Profile</h1>
-        <div className="profile-buttons">
-          <button onClick={() => setActiveSection('saved')}>Saved Recipes</button>
-          <button onClick={() => setActiveSection('created')}>Created Recipes</button>
-        </div>
-        <div className="create-recipe-button">
-          <button>Create Recipe</button>
-        </div>
-        {renderSection()}
+        <button className="create-recipe-button">Create Recipe</button>
+      </div>
+      <div className="profile-tabs">
+        <button
+          className={`tab-button ${activeSection === "saved" ? "active" : ""}`}
+          onClick={() => setActiveSection("saved")}
+        >
+          Saved Recipes
+        </button>
+        <button
+          className={`tab-button ${
+            activeSection === "created" ? "active" : ""
+          }`}
+          onClick={() => setActiveSection("created")}
+        >
+          Created Recipes
+        </button>
+      </div>
+      <div className="profile-container">
+        <div className="profile-content">{renderSection()}</div>
       </div>
     </div>
   );
