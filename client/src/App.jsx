e4b78ba8391922af8ed450ca2b67ddf697449f26
cassidy <cassidykovell@Cassidys-iMac.lan>
2024-05-30 21:06:35 -0700

    changed color pallet and cleaned up the profile page and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="layout">
+    <div>
       <header className="header">
         <Header />
         <Nav />
