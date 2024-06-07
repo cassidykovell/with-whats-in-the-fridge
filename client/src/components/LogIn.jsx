d143a7a2ac47bdc1aba294c0a5d3aeb4b55861cb
cassidy <cassidykovell@Cassidys-iMac.lan>
2024-06-06 20:10:45 -0700

    added log in and sign up for user

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -1,35 +1,65 @@
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
+
+import Auth from '../../utils/auth';
+
 
 const LogIn = () => {
-  const [loginUsername, setLoginUsername] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  const handleLogin = () => {
-    // Handle login logic here
-    console.log("Logging in...");
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      email: "",
+      password: "",
+    });
   };
 
   return (
-        <div className="login-form">
-        <h2 id="log-head">Login</h2>
-        <input
-          className="input"
-          type="text"
-          placeholder="Username"
-          value={loginUsername}
-          onChange={(e) => setLoginUsername(e.target.value)}
-        />
-        <input
-          className="input"
-          type="password"
-          placeholder="Password"
-          value={loginPassword}
-          onChange={(e) => setLoginPassword(e.target.value)}
-        />
-        <button onClick={handleLogin} id="log-btn">Login</button>
-
-      </div>
-    
+    <div className="login-form">
+      <h2 id="log-head">Login</h2>
+      <input
+        className="input"
+        name="email"
+        type="email"
+        placeholder="Email"
+        value={formState.email}
+        onChange={handleChange}
+      />
+      <input
+        className="input"
+        placeholder="Password"
+        name="password"
+        type="password"
+        value={formState.password}
+        onChange={handleChange}
+      />
+      <button onClick={handleFormSubmit} id="log-btn">
+        Login
+      </button>
+    </div>
   );
 };
 
